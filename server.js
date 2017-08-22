8571a66e6a1ca34bf0cf18cf7258a1f1f6660ace
rakeshtati <rakeshtati@gmail.com>
2017-08-22 22:46:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Article one requested will bes served here');
+  es.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
